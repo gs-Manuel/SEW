ed7994367a0e176e63cc650de9622a88614f9855
Manuel <gs_manuel@outlook.es>
2023-12-01 00:19:16 +0100

    Ejercicio4JS

--- js/fondo.js
+++ js/fondo.js
@@ -1,38 +1,34 @@
 "use strict";
-class fondo{
+class Fondo{
     constructor( pais){
         this.nombre = pais.getNombre();
-        this.capital = paiss.getCapital();
+        this.capital = pais.getCapital();
         this.coordenadas = pais.getCoordenadasCapital();
     }
-    getNombre(){
-        return this.nombre;
-    }
-    getCapital(){
-        return this.capital;
-    }
-    getCoordenadasCapital(){
-        return this.coordenadas;
-    }
-    getFotos() {
-        var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-        $.getJSON(flickrAPI, 
-                {
-                    tags: this.getCapital(),
-                    tagmode: "any",
-                    format: "json"
-                })
-            .done(function(data) {
-                    $.each(data.items, function(i,item ) {
-                        $("<img />").attr( "src", item.media.m).appendTo( "#imagenes" );
-                        if ( i === 20 ) {
-                                return false;
-                        }
-                    });
-        });
-    };
+    getImagen(callback) {
+        const apiKey = "449519db638408c458d94b70d99d04ba";
+        const apiUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${this.capital}&format=json&nojsoncallback=1`;
 
+        $.ajax({
+            url: apiUrl,
+            method: 'GET',
+            dataType: 'json',
+            success: function (data) {
+              // Verificar si se obtuvieron resultados
+              if (data.photos && data.photos.photo.length > 0) {
+                const photo = data.photos.photo[0];
+                const imageUrl = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
+    
+                callback(imageUrl);
+              } else {
+                console.error(`No se encontraron imágenes para la capital de ${pais}.`);
+              }
+            },
+            error: function (error) {
+              console.error('Error en la solicitud AJAX:', error);
+            }
+          });
+        }
+    //Secreto: 1b710fc80ffa252f
+    //Background de fondo mediante propiedad css
 }
-var pais = new Pais("Marruecos", "Rabat", 36000000);
-var fondo = new fondo( pais);
-fondo.getFotos();
--- js/pais.js
+++ js/pais.js
@@ -49,12 +49,3 @@ class Pais{
             `)
     }
 }
-var pais = new Pais("Marruecos", "Rabat", 36000000);
-document.write("<h3>"+pais.getNombre()+"</h3>")
-document.write("<h4> Información principal</h4>")
-document.write("<p> Capital: "+pais.getCapital()+"</p>")
-document.write("<p> Número de habitantes: "+pais.getCantidadPoblacion()+" personas</p>")
-pais.escribirCoordenadasCapital();
-document.write("<h4> Más información</h4>")
-document.write(pais.getInformacionSecundario());
-document.close();
