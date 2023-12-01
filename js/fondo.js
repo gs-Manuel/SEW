b2a650fa81330e1828df728f432b51f70c183c00
Manuel <gs_manuel@outlook.es>
2023-12-01 17:03:13 +0100

    Ejercicio 5JS

--- js/fondo.js
+++ js/fondo.js
@@ -5,21 +5,28 @@ class Fondo{
         this.capital = pais.getCapital();
         this.coordenadas = pais.getCoordenadasCapital();
     }
-    getImagen(callback) {
+    getImagen() {
         const apiKey = "449519db638408c458d94b70d99d04ba";
-        const apiUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${this.capital}&format=json&nojsoncallback=1`;
+        const apiUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${this.capital}&format=json&nojsoncallback=true`;
 
-        $.ajax({
+        $.getJSON({
             url: apiUrl,
             method: 'GET',
             dataType: 'json',
             success: function (data) {
               // Verificar si se obtuvieron resultados
-              if (data.photos && data.photos.photo.length > 0) {
-                const photo = data.photos.photo[0];
-                const imageUrl = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
-    
-                callback(imageUrl);
+              if (data.stat='ok') {
+                console.log(data);
+                const photo = data.photos.photo[Math.floor(Math.random() * data.photos.photo.length) ];
+                const imageUrl = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_b.jpg`;
+                $("article").css({
+                  "background-image": `url('${imageUrl}')`,
+                  "background-repeat": "no-repeat",
+                  'height': '100vh',
+                  "background-size": "cover",
+                  "background-position": "center center"
+                  
+              });
               } else {
                 console.error(`No se encontraron imágenes para la capital de ${pais}.`);
               }
--- js/pais.js
+++ js/pais.js
@@ -40,6 +40,7 @@ class Pais{
       return listaHTML;
     }
     escribirCoordenadasCapital() {
+        //Escribir despues del titulo de más informacion
         document.write("<p> Coordenadas de la capital</p>")
         document.write(`
             <ul>
@@ -48,4 +49,47 @@ class Pais{
             </ul>
             `)
     }
+
+    obtenerPrevisionTiempo() {
+            const apiKey = "af570203c4f84141406f2b72dc283547";
+            const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${this.getCoordenadasCapital()[0]}&lon=${this.getCoordenadasCapital()[1]}&units=metric&appid=${apiKey}`;
+
+            $.getJSON({
+                url: apiUrl,
+                method: 'GET',
+                dataType: 'json',
+                success: data =>{
+                    console.log("Previsión del tiempo:", data);
+                    this.imprimirPrevisionTiempo(data);
+                },
+                error: error => {
+                    console.error(error);
+                }
+            });
+    }
+    imprimirPrevisionTiempo(data) {
+        var minTempDay = Number.POSITIVE_INFINITY;
+        var maxTempDay = -100000;
+        data.list.forEach(element => {
+             minTempDay = element.main.temp_min < minTempDay ? element.main.temp_min : minTempDay;
+             maxTempDay = element.main.temp_max > maxTempDay ? element.main.temp_max : maxTempDay;
+            if(element.dt_txt.includes("18:00:00")){ //Si es la hora de las 15:00
+                const section = $("<section>");
+                section.append(`<h4>${element.main.temp} °C</h4>`);
+                section.append(`<p>Temperatura mínima: ${minTempDay} °C</p>`);
+                section.append(`<p>Temperatura máxima: ${maxTempDay} °C</p>`);
+                section.append(`<p>Presión: ${element.main.pressure} °C</p>`);
+                section.append(`<p>Humedad: ${element.main.humidity} %</p>`);
+                section.append(`<p>Cantidad de Lluvia: ${element.rain ? element.rain['3h'] : 0} mm</p>`);
+                const iconUrl = `https://openweathermap.org/img/w/${element.weather[0].icon}.png`;
+                section.append(`<img src="${iconUrl}" alt="Icono del tiempo">`);
+                $("main").append(section);
+             
+                minTempDay = Number.POSITIVE_INFINITY;
+                maxTempDay = -100000;
+            }
+        });
+    }
+
+
 }
